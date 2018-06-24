54de518043aac664de34eb0c05b6a35469776e11
Stephanie Blondet <stephanieblondet@Stephanies-iMac.fios-router.home>
2018-06-24 09:33:29 -0400

    Creating layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+
 import { AppComponent } from './app.component';
+import { PrevNextComponent } from './prev-next/prev-next.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrevNextComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatGridListModule,
+    MatCardModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
